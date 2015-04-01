faa4648960cc5c34a7209301e6fbc8d6d8c81ce5
Mats Lundström <mats.a.lundstrom@gmail.com>
2015-04-01 07:28:26 +0200

    Added jshint and stylish

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
+gulp.task('jshint', function() {
+    'use strict';
+    gulp.src(config.jsPath)
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('default', function() {
+
+});
