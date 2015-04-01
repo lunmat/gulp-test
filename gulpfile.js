74f3555e3f27efbacf14c1b85d9828a85c68242f
Mats Lundström <mats.a.lundstrom@gmail.com>
2015-04-01 21:50:12 +0200

    Added more gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var sass = require('gulp-sass');
+var rimraf = require('rimraf');
 
 gulp.task('jshint', function() {
     'use strict';
-    gulp.src(config.jsPath)
+    gulp.src('./src/**/*.js', '!./bower_components/**/*', '!./tests/**/*')
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
+});
+
+
+gulp.task('sass', function () {
+    gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dest/css'));
+});
+
+gulp.task('clean', function (cb) {
+    rimraf('./dest', cb);
 });
 
 gulp.task('default', function() {
